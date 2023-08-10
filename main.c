91ef226300337ee6e2b9b508f21947c49086b42e
Liveset1 <yousifwilliam12345@outlook.com>
2023-08-10 18:21:32 -0400

    Initial

--- main.c
+++ main.c
@@ -23,20 +23,24 @@ typedef struct {
     // Window Properties / Configurable Values
     const char *applicationName;
     const char *engineName;
-    const char *window_title;
+    const char *windowTitle;
     int width, height;
     bool resizable;
-    bool window_fullscreen;
+    bool windowFullScreen;
 
     // GLFW
     GLFWwindow *window;
-    GLFWmonitor *window_monitor;
+    GLFWmonitor *windowMonitor;
 
     // Vulkan
     VkAllocationCallbacks *allocator;
     VkInstance instance;
     uint32_t api_version;
-
+    VkPhysicalDevice physicalDevice;
+    VkSurfaceKHR surface;
+    uint32_t queueFamily;
+    VkDevice device;
+    VkQueue queue;
 } State;
 
 void setupErrorHandling() {
@@ -49,10 +53,14 @@ void initializeWindow(State *state) {
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
     glfwWindowHint(GLFW_RESIZABLE, state->resizable);
 
-    if (state->window_fullscreen) {
-        state->window_monitor = glfwGetPrimaryMonitor();
+    if (state->windowFullScreen) {
+        state->windowMonitor = glfwGetPrimaryMonitor();
+        // Setting Width and Height to Monitor Resolution
+        GLFWvidmode *mode = glfwGetVideoMode(state->windowMonitor);
+        state->width = mode->width;
+        state->height = mode->height;
     }
-    state->window = glfwCreateWindow(state->width, state->height, state->window_title, state->window_monitor, NULL);
+    state->window = glfwCreateWindow(state->width, state->height, state->windowTitle, state->windowMonitor, NULL);
 }
 
 void createVKInstance(State *state) {
@@ -75,10 +83,67 @@ void createVKInstance(State *state) {
     PANIC(vkCreateInstance(&instanceCreateInfo, state->allocator, &state->instance), "Could not create VK Instance!");
 }
 
+void selectPhysicalDevice(State *state) {
+    uint32_t count;
+    PANIC(vkEnumeratePhysicalDevices(state->instance, &count, NULL), "Could not enumerate physical devices count!");
+    PANIC(count == 0, "Could not find a vulkan supported physical device!");
+    PANIC(vkEnumeratePhysicalDevices(state->instance, &count, &state->physicalDevice), "Could not enumerate physical devices count!");
+}
+
+void createSurface(State *state) {
+    PANIC(glfwCreateWindowSurface(state->instance, state->window, state->allocator, &state->surface), "Could not create window surface!");
+}
+
+void selectQueueFamily(State *state) {
+    state->queueFamily = UINT32_MAX;
+
+    uint32_t count;
+    vkGetPhysicalDeviceQueueFamilyProperties(state->physicalDevice, &count, NULL);
+    VkQueueFamilyProperties *queueFamilies = malloc(count*sizeof(VkQueueFamilyProperties));
+    PANIC(queueFamilies == NULL, "Could not allocate memory for queue families!")
+    vkGetPhysicalDeviceQueueFamilyProperties(state->physicalDevice, &count, queueFamilies);
+
+    for (int queueFamilyIndex = 0; queueFamilyIndex < count; ++queueFamilyIndex) {
+        VkQueueFamilyProperties properties = queueFamilies[queueFamilyIndex];
+        if (properties.queueFlags & VK_QUEUE_GRAPHICS_BIT
+        && glfwGetPhysicalDevicePresentationSupport(state->instance, state->physicalDevice, queueFamilyIndex)) {
+            state->queueFamily = queueFamilyIndex;
+            break;
+        }
+    }
+    PANIC(state->queueFamily == UINT32_MAX, "Could not find a suitable Queue Family!");
+    free(queueFamilies);
+}
+
+void createVKDevice(State *state) {
+    PANIC(vkCreateDevice(state->physicalDevice, &(VkDeviceCreateInfo) {
+        .sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO,
+       .pQueueCreateInfos =  &(VkDeviceQueueCreateInfo) {
+                  .sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO,
+                  .queueFamilyIndex = state->queueFamily,
+                  .queueCount = 1,
+                  .pQueuePriorities = &(float){1.0},
+        },
+        .queueCreateInfoCount = 1,
+        .enabledExtensionCount = 1,
+        .ppEnabledExtensionNames = &(const char*) {VK_KHR_SWAPCHAIN_EXTENSION_NAME}
+    }, state->allocator, &state->device), "Could not create VK Device and Queues!");
+}
+
+void getVKQueue(State *state) {
+    vkGetDeviceQueue(state->device, state->queueFamily, 0, &state->queue);
+}
+
 void initializeEngine(State *state) {
     setupErrorHandling();
     initializeWindow(state);
     createVKInstance(state);
+
+    selectPhysicalDevice(state);
+    createSurface(state);
+    selectQueueFamily(state);
+    createVKDevice(state);
+    getVKQueue(state);
 }
 
 void engineLoop(State *state) {
@@ -88,6 +153,8 @@ void engineLoop(State *state) {
 }
 
 void engineCleanup(State *state) {
+    vkDestroyDevice(state->device, state->allocator);
+    vkDestroySurfaceKHR(state->instance, state->surface, state->allocator);
     glfwDestroyWindow(state->window);
     vkDestroyInstance(state->instance, state->allocator);
 }
@@ -97,11 +164,11 @@ int main() {
     State state = {
             .applicationName = name,
             .engineName = name,
-            .window_title = name,
+            .windowTitle = name,
             .width = 800,
             .height = 600,
             .resizable = true,
-            .window_fullscreen = false,
+            .windowFullScreen = false,
             .api_version = VK_API_VERSION_1_0,
     };
 
